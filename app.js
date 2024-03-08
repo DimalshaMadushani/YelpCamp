d3c61cb9545a70934db219088288241832f5fdcf
dimalsha <madushaniagd@gmail.com>
2024-03-08 17:24:32 +0530

    section-43-yelpcamp
    upto Using Joi schema validation

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+
+const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
+
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const { title } = require('process');
 
 //connect to databse
 // mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp',)
@@ -40,42 +46,82 @@ app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
     res.render('home')
 });
-app.get('/campgrounds', async (req, res) => {
+
+app.get('/campgrounds', catchAsync(async (req, res,next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
-});
+}));
+
+
+//create a new campground
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res,next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid campground data',400)
+    //this is just a schema for joi to validate
+    const campgroundSchema = Joi.object({
+        campground : Joi.object({
+            title : Joi.string().required(),
+            price : Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required()
+            
+        }).required()
+    })
+    //
+    const {error} = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
+    // console.log(result)
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}));
 
-app.get('/campgrounds/:id', async (req, res,) => {
+//show a campground
+app.get('/campgrounds/:id', catchAsync(async (req, res,next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+
+//edit a campground
+app.get('/campgrounds/:id/edit', async (req, res,next) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-})
+});
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res,next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`)
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res,next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}));
+
+//app.all is a method provided by Express that is used to handle all types of HTTP requests 
+//(GET, POST, PUT, DELETE, etc.) at a particular route. In this case, the route is specified as '*', which acts as a wildcard, matching any path.
+app.all('*',(req,res,next) => {
+    // res.status(404).send("404 !!")
+    next(new ExpressError('Page Not Found',404))
 })
 
+//express error handling midleware
+app.use((err,req,res,next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oh No, Something went wrong!'
+    //rendering the error.ejs file
+    res.status(statusCode).render('error',{err})
+    // res.send("oh nooo")
+})
 
 
 app.listen(3000, () => {
